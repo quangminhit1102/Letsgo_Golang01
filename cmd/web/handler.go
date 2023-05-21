070575e4b0c05581940637a1b5bc7baaac966889
Nguyễn Quang Minh <quangminhit1102@gmail.com>
2023-05-21 17:20:02 +0700

    Page 70 Ongoing - Fighting

---
+++ cmd/web/handler.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"html/template" // New import
+	"log"           // New import
+	"net/http"
+	"strconv"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	files := []string{
+		"./ui/html/pages/home.tmpl.html",
+		"./ui/html/base.tmpl.html",
+		"./ui/html/partials/nav.tmpl.html",
+	}
+
+	ts, err := template.ParseFiles(files...) // read the template
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil) // Excecute the template, nil (can be dynamic data)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+}
+
+// Add a snippetView handler function.
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
+}
+
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		w.WriteHeader(405)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	w.Write([]byte("Create a new snippet..."))
+}
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,55 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"log"
 	"net/http"
-	"strconv"
+	"os"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-	w.Write([]byte("Hello from the other side!"))
-}
-
-// Add a snippetView handler function.
-func snippetView(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Query().Get("id"))
-	if err != nil || id < 1 {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
-}
-
-// Add a snippetCreate handler function.
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-		// If it's not, use the w.WriteHeader() method to send a 405 status
-		// code and the w.Write() method to write a "Method Not Allowed"
-		// response body. We then return from the function so that the
-		// subsequent code is not executed.
-		w.Header().Set("Allow", "POST")
-		w.WriteHeader(405)
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	w.Write([]byte("Create a new snippet..."))
-}
 func main() {
 	mux := http.NewServeMux()
+	addr := flag.String("addr", ":4000", "HTTP network address") // Get Flag Variable
+	flag.Parse()                                                 // Parse the Flag Variable
+
+	fileServer := http.FileServer(http.Dir("./ui/static/")) // Static File Server
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)                  // Info Log
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile) // Error Log
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
-	// Use the http.ListenAndServe() function to start a new web server. We pass in
-	// two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created. If http.ListenAndServe() returns an error
-	// we use the log.Fatal() function to log the error message and exit. Note
-	// that any error returned by http.ListenAndServe() is always non-nil.
-	log.Println("Starting server on :4000")
+
+	infoLog.Printf("Starting server on :%s", *addr) // flag return a Pointer
 	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+
+	errorLog.Fatal(err)
 }
